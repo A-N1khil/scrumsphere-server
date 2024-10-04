aeba2f19b4aceaf552ea8d3469e12c529137061f
A-N1khil <nikhilanand1006@gmail.com>
2024-10-04 19:27:19 -0400

    feat: init commit

---
+++ src/main/java/com/anikhil/scrumsphere/ScrumSphereApplication.java
@@ -0,0 +1,13 @@
+package com.anikhil.scrumsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumSphereApplication.class, args);
+	}
+
+}
